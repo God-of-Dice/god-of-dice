d76189be5672954bafc05c548a0d565a4ec01e0a
mhdiiilham <muhd.iiilham@gmail.com>
2020-01-23 20:12:01 +0700

    -	Adding create and join room, 	if room not exist, will sent info to client. - At store/index.js add createRoom and joinroom actions

---
+++ src/components/test.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <h2>Nama room</h2>
+    nama player:
+    <input type="text" v-model="playerName"><br>
+    nama room:
+    <input type="text" v-model="roomName"><br>
+    <button @click="create" >Create</button>
+    <hr>
+    Join room:
+    <input type="text" v-model="roomName">
+    <button @click="join">JOIN</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello world',
+      roomName: '',
+      playerName: '',
+    };
+  },
+  methods: {
+    join() {
+      const payload = {
+        roomName: this.roomName,
+        player: this.playerName,
+      };
+      this.$store.dispatch('joinroom', payload);
+    },
+    create() {
+      const payload = {
+        roomName: this.roomName,
+        player: this.playerName,
+      };
+      this.$store.dispatch('createRoom', payload);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
